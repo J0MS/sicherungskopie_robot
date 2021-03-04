eea8193c9eaad3c58aeb5caa47382a1553054f4a
Jose Manuel S <subliminal@ciencias.unam.mx>
2021-03-03 22:29:50 -0600

    Update env variables configuration

--- src/sicherungskopie_robot/config.py
+++ src/sicherungskopie_robot/config.py
@@ -6,6 +6,7 @@ import configargparse
 class AppCommands(str,Enum):
     BACKUP = "backup"
     RESTORE = "restore"
+    LS="ls"
 
 # class AppConfig():
 class AppConfig(Protocol):
@@ -13,6 +14,16 @@ class AppConfig(Protocol):
     command: str
     backup: str
     restore: str
+    object:str
+    output_dir:str
+    sign_key: str
+    encrypt_key: str
+    service_account: str
+    service_key: str
+    service_bucket: str
+    service_folder: str
+
+
 
 def parse_args() -> AppConfig:
     parser = configargparse.ArgumentParser()
@@ -21,18 +32,33 @@ def parse_args() -> AppConfig:
     subparsers = parser.add_subparsers(dest="command", help="Sub-command to run")
 
     subparser = subparsers.add_parser(AppCommands.BACKUP.value, help="Backup an object")
-    # subparser.add_argument("--redash-endpoint", env_var="REDASH_ENDPOINT")
+    subparser.add_argument("--sign_key", env_var="SIGN_KEY")
+    subparser.add_argument("--encrypt_key", env_var="ENCRYPT_KEY")
+    subparser.add_argument("--time_delta", env_var="TIME_DELTA")
+    subparser.add_argument("--object",  type=str, required=True )
+    subparser.add_argument("--service_account", env_var="SERVICE_ACCOUNT")
+    subparser.add_argument("--service_key", env_var="SERVICE_KEY")
+    subparser.add_argument("--service_bucket", env_var="SERVICE_BUCKET")
+    subparser.add_argument("--service_folder", env_var="SERVICE_FOLDER")
+
 
     subparser = subparsers.add_parser(AppCommands.RESTORE.value, help="Restore an object from backup")
+    subparser.add_argument("--sign_key", env_var="SIGN_KEY")
+    subparser.add_argument("--encrypt_key", env_var="ENCRYPT_KEY")
     subparser.add_argument("--object", type=str, required=True)
-    subparser.add_argument("--output-dir", type=str, required=True)
+    subparser.add_argument("--output_dir", type=str, required=True)
+    subparser.add_argument("--service_folder", env_var="SERVICE_FOLDER")
 
-    # subparser.add_argument(
-    #     "--kpis-service-connection-string",
-    #     type=str,
-    #     required=True,
-    #     env_var="POSTGRES_URI_MICROSERVICE",
-    # )
+
+
+    subparser = subparsers.add_parser(AppCommands.LS.value, help="Get a list of objects from a service bucket-folder")
+    subparser.add_argument("--sign_key", env_var="SIGN_KEY")
+    subparser.add_argument("--encrypt_key", env_var="ENCRYPT_KEY")
+    subparser.add_argument("--service_account", env_var="SERVICE_ACCOUNT")
+    subparser.add_argument("--service_key", env_var="SERVICE_KEY")
+    subparser.add_argument("--service_bucket", env_var="SERVICE_BUCKET")
+    subparser.add_argument("--service_folder", env_var="SERVICE_FOLDER")
+
 
     args = cast(AppConfig, parser.parse_args())
 
@@ -41,18 +67,3 @@ def parse_args() -> AppConfig:
         sys.exit(-1)
 
     return args
-
-
-
-# parser = argconfig.ArgumentParser(description='Worker config',config='./config.yaml')
-# parser.add_argument('-b','--backup', type=str, default='testing',
-#                     help='Backup an object (default=testing, config=test)')
-# parser.add_argument('-r','--restore', type=str, default='testing',
-#                     help='Restore an object (default=testing, config=test)')
-
-
-# args = parser.parse_args()
-
-# print('foo:',args.backup)
-# print('bar:',args.restore)
-print('Melanie X')
--- src/sicherungskopie_robot/main.py
+++ src/sicherungskopie_robot/main.py
@@ -1,20 +1,33 @@
+import logging
+from pathlib import Path
+from dotenv import load_dotenv
 from sicherungskopie_robot.config import AppCommands, parse_args
+from sicherungskopie_robot.tasks.backup import *
+from sicherungskopie_robot.tasks.restore import *
 
 def main():
     # load_dotenv()
+    env_path = Path('.') / '.env'
+    load_dotenv(dotenv_path=env_path)
 
     args = parse_args()
 
-    # forecast_parameters_execution_id = (uuid4(),)
 
-    # logging.basicConfig(level="DEBUG" if args.debug else "INFO")
+    logging.basicConfig(level="DEBUG" if args.debug else "INFO")
 
-    # logging.info(f"Command: {args.command}")
+    logging.info(f"Command: {args.command}")
 
-    if args.command == AppCommands.RESTORE:
+    if args.command == AppCommands.BACKUP:
+        print('Backup')
+        print('xxx',args)
+        backup()
+
+    elif args.command == AppCommands.RESTORE:
         print(args)
         print('Restore')
         return
 
-
+    elif args.command == AppCommands.LS:
+        print(args)
+        print('List')
 # print('From main')
