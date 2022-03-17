f23171ed9c6c787854b2cb716114c0fe27f868c3
José Manuel S <s5po@gmx.com>
2022-03-16 21:45:18 -0600

    Parametrized origin-dest folders

--- duplicity_local.sh
+++ duplicity_local.sh
@@ -6,23 +6,38 @@ echo "Starten Backup."
 #read B2_ACCOUNT B2_KEY ENC_KEY SGN_KEY < config/secrets.config
 # Read source directories to backup
 #read CINVESTAV_DIR UNAM_DIR PROYECTS_DIR LOG_DIR < config/sources_dir.config
-EXTERNAL_HDD_DOCUMENTS='file:///run/media/icarus/HD710 PRO/backup'
+#EXTERNAL_HDD_DOCUMENTS='file:///run/media/icarus/HD710 PRO/backup'
+#DOCUMENTS_DIR="$HOME/Documents"
+
+
 DOCUMENTS_DIR="$HOME/Documents"
+
+
 
 readarray -t sources < config/sources.config
 
-for path in "${sources[@]}"
+
+DESTINATION=${sources[0]}
+
+if [ ! -d "${DESTINATION}" ] 
+then
+	echo "Directory /path/to/dir DOES NOT exists." 
+	exit 9999 # die with error code 9999
+else
+	echo "Setup ready"
+fi
+
+for path in "${sources[@]:1:2}"
 do
-	echo "$path"
 	folder=${path%/*}
 	dest_dir="${folder##*/}"
-	echo "$EXTERNAL_HDD_DOCUMENTS/$dest_dir"
+	echo "$DESTINATION/$dest_dir"
 
 	# Preform the backup, make a full backup if it's been over 15 days
-	duplicity --no-encryption --progress  --full-if-older-than 15D  $path "$EXTERNAL_HDD_DOCUMENTS/$dest_dir" 
+#	duplicity --no-encryption --progress  --full-if-older-than 15D  $path "$DESTINATION/$dest_dir" 
 
 	#Incremental backup
-	duplicity --no-encryption --progress incr $path "$EXTERNAL_HDD_DOCUMENTS/$dest_dir" 
+#	duplicity --no-encryption --progress incr $path "$DESTINATION/$dest_dir" 
 done
 
 #B2_BUCKET="GreenFolder"
