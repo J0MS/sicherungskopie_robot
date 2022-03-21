a6cc281762cccd6934e102dd3478c2111c81be05
José Manuel S <s5po@gmx.com>
2022-03-20 23:10:14 -0600

    Remote sync first version

--- duplicity_remote.sh
+++ duplicity_remote.sh
@@ -35,7 +35,7 @@ export PASSPHRASE=(`kdialog --title "Pin entry" --password "Please enter the pri
 GPG_KEY=${sources[0]}
 B2_KEY_ID=${sources[1]}
 B2_APP_KEY=${sources[2]}
-LOG_DIR="log/"
+LOG_DIR="log/remote/"
 LOG_DATE=$(date '+%Y-%m-%d')
 LOG_FILE="remote_backup_$LOG_DATE.log"
 
@@ -43,49 +43,33 @@ LOG_FILE="remote_backup_$LOG_DATE.log"
 for path in "${sources[@]:3}"
 	do
 		folder=${path%/*}
-		dest_dir="${folder##*/}"
+		DEST_DIR="${folder##*/}"
 		DEST_BUCKET=${path##*:}
 		echo "Synchronisieren  $folder zu $DEST_BUCKET (B2 bucket)"
+		echo $path
 		echo $folder
 		echo $DEST_BUCKET
+		echo $DEST_DIR
 
 		# Preform the backup, make a full backup if it's been over 15 days
-#		duplicity --no-encryption --progress  --full-if-older-than 15D  $path "$DESTINATION/$dest_dir" 
-		duplicity --progress --sign-key $GPG_KEY --encrypt-key $GPG_KEY --full-if-older-than 15D $path b2://${B2_KEY_ID}:${B2_APP_KEY}@${B2_BUCKET}
+		duplicity --progress --sign-key $GPG_KEY --encrypt-key $GPG_KEY --full-if-older-than 15D $folder b2://${B2_KEY_ID}:${B2_APP_KEY}@${DEST_BUCKET}/${DEST_DIR}
 
 		#Incremental backup
-#		duplicity --no-encryption --progress incr $path "$DESTINATION/$dest_dir" 
+		duplicity --progress incr --sign-key $GPG_KEY --encrypt-key $GPG_KEY $folder b2://${B2_KEY_ID}:${B2_APP_KEY}@${DEST_BUCKET}/${DEST_DIR}
 
 		#Remove files older than 90 days
-		#duplicity --progress  remove-older-than 30D --force b2://${B2_ACCOUNT}:${B2_KEY}@${B2_BUCKET}/${DEST_DIR1}
+		#duplicity --progress --sign-key $SGN_KEY --encrypt-key $ENC_KEY remove-older-than 30D --force b2://${B2_KEY_ID}:${B2_APP_KEY}@${DEST_BUCKET}/${DEST_DIR}
 
 		#List of current files in backup
-#		duplicity --progress list-current-files "$DESTINATION/$dest_dir" >> "$LOG_DIR/$LOG_FILE"
+		duplicity --progress list-current-files b2://${B2_KEY_ID}:${B2_APP_KEY}@${DEST_BUCKET}/${DEST_DIR}>> "$LOG_DIR/$LOG_FILE"
 
 	done
-
 
 
 
 unset GPG_KEY
 unset B2_KEY_ID
 unset B2_APP_KEY
-
-
-
-#DEST_DIR1="BioTec"
-
-# Preform the backup, make a full backup if it's been over 15 days
-
-#Incremental backup
-#duplicity --progress incr --sign-key $SGN_KEY --encrypt-key $ENC_KEY ${CINVESTAV_DIR} b2://${B2_ACCOUNT}:${B2_KEY}@${B2_BUCKET}/${DEST_DIR1}
-
-#Remove files older than 90 days
-#duplicity --progress --sign-key $SGN_KEY --encrypt-key $ENC_KEY remove-older-than 30D --force b2://${B2_ACCOUNT}:${B2_KEY}@${B2_BUCKET}/${DEST_DIR1}
-
-#duplicity --progress list-current-files b2://${B2_ACCOUNT}:${B2_KEY}@${B2_BUCKET}/${DEST_DIR1}>> "$LOG_DIR/Cinvestav_backup.log"
-
-
 
 
 
